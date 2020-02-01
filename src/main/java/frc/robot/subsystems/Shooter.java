5d25b5c1220cf4afa745f65e870ae71ac6652ec4
Locke Adams <lockeadams@gmail.com>
2020-01-31 19:40:32 -0500

    Fixed style errors.

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,9 +1,9 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -62,7 +62,7 @@ public class Shooter extends SubsystemBase {
         SmartDashboard.putNumber("Right shooter rpm", getRightVelocity());
     }
 
-    /** 
+    /**
      * Configures gains for SRX closed loop controller.
      */
     private void controllerInit() {
